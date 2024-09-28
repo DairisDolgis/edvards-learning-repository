08661ef528a685b26aacb5d7bc01939e268ca770
EdvardsV <storm.heart2009@gmail.com>
2024-09-28 20:24:07 +0300

    teksts

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React Edvards</h1>
+      <h1>edvards-learning-repository</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
